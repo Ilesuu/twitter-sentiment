e0f9cc9e7f528b11a9461906f0e7e322d6850b61
Ilkka Suutarinen <ilkka.suutarinen@gmail.com>
2021-01-15 14:34:00 +0200

    Added sentiment analysis method outlines

---
+++ sentiment_analysis.py
@@ -0,0 +1,14 @@
+
+KEYWORDS_POSITIVE = ["good", "strong","great", "nice", "awesome", "cool", "thank", "thanks"]
+KEYWORDS_NEGATIVE = ["bad", "weak", "terrible", "shit"]
+
+
+def analyze_text(self, text):
+    positive_words = 0
+    negative_words = 0
+
+def analyze_dataset(self, dataset):
+    positive_amount = 0
+    negative_amount = 0
+
+
