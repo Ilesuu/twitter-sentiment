f15a54187bd62beb0eea1ee21ee4bfb6251ddf6f
Ilkka Suutarinen <ilkka.suutarinen@gmail.com>
2021-01-15 15:11:55 +0200

    Added get_tweet_texts to TweetScraper

--- tweet_scraper.py
+++ tweet_scraper.py
@@ -3,7 +3,6 @@
 import tweepy
 
 
-
 class TweetScraper:
     def __init__(self, key):
         f = open(key, "r")
@@ -17,3 +16,13 @@ class TweetScraper:
             return True
         except tweepy.TweepError:
             return False
+
+    def get_tweet_texts(self, text_query, count):
+        results = []
+        tweets = self.__api.search(q=text_query, lang="en", count=count, tweet_mode='extended')
+        for tweet in tweets:
+            try:    # test if retweet
+                results.append(tweet.retweeted_status.full_text)
+            except AttributeError:  # Not a retweet
+                results.append(tweet.full_text)
+        return results
